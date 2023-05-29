914287d21a76d383abff5d9a078091cbb6808c63
wenjun <laojiaodaoniu@126.com>
2023-05-29 11:25:29 +0800

    test: with openzepplin test-helper

--- test/Box.test.ts
+++ test/Box.test.ts
@@ -1,23 +1,36 @@
-import { ethers } from 'hardhat'
+import { expect } from 'chai'
 
-const { expect } = require('chai')
+// @ts-ignore
+import { BN, expectEvent, expectRevert } from '@openzeppelin/test-helpers'
 
-describe('Box', () => {
-  let Box: any
+let Box = artifacts.require('Box')
+
+// @ts-ignore
+contract('Box', ([owner, other]) => {
   let box: any
-
-  before(async () => {
-    Box = await ethers.getContractFactory('Box')
-  })
+  const value = new BN('42')
 
   beforeEach(async () => {
-    box = await Box.deploy();
-    await box.deployed()
+    box = await Box.new({ from: owner });
   })
 
   it('retrieve returns a value previously stored', async () => {
-    await box.store(42)
+    await box.store(value, { from: owner })
 
-    expect((await box.retrieve()).toString()).to.equal('42')
+    //@ts-ignore
+    expect((await box.retrieve()).toString()).to.be.bignumber.equal('42')
   })
+
+  it('store emits an event', async () => {
+    const receipt = await box.store(value, { from: owner })
+    expectEvent(receipt, 'ValueChanged', { value: value })
+  })
+
+  it('non owner cannot store a value', async () => {
+    await expectRevert(
+      box.store(value, { from: other }),
+      'Ownable: caller is not the owner'
+    )
+  })
+
 })
