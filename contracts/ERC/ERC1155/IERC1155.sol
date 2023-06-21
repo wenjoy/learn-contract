c86aa23485bf1a5ecdec61714fcb5e082d95ed8c
wenjun <laojiaodaoniu@126.com>
2023-06-21 16:03:04 +0800

    fix: add ERC1155 interface specification

---
+++ contracts/ERC/ERC1155/IERC1155.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+/* is ERC165 */ interface IERC1155 {
+    event TransferSingle(
+        address indexed _operator,
+        address indexed _from,
+        address indexed _to,
+        uint256 _id,
+        uint256 _value
+    );
+    event TransferBatch(
+        address indexed _operatoer,
+        address indexed _from,
+        address indexed _to,
+        uint256[] _ids,
+        uint256[] _values
+    );
+    event ApprovalForAll(
+        address indexed _owner,
+        address indexed _operator,
+        bool _approved
+    );
+    event URI(string _value, uint256 indexed _id);
+
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _id,
+        uint256 _value,
+        bytes calldata _data
+    ) external;
+
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256[] calldata _ids,
+        uint256[] calldata _values,
+        bytes calldata _data
+    ) external;
+
+    function balanceOf(
+        address _onwer,
+        uint256 _id
+    ) external view returns (uint256);
+
+    function balanceOfBatch(
+        address[] calldata _owners,
+        uint256[] calldata _ids
+    ) external view returns (uint256[] memory);
+
+    function setApprovalForAll(address _operator, bool _approved) external;
+
+    function isApprovedForAll(
+        address _owner,
+        address _operator
+    ) external view returns (bool);
+}
+
+interface IERC1155TokenReceiver {
+    function onERC1155Received(
+        address _operator,
+        address _from,
+        uint256 _id,
+        uint256 _value,
+        bytes calldata _data
+    ) external returns (bytes4);
+
+    function onERC1155BatchReceived(
+        address _operator,
+        address _from,
+        uint256[] calldata _ids,
+        uint256[] calldata _values,
+        bytes calldata _data
+    ) external returns (bytes4);
+}
